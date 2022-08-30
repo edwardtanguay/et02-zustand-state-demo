6034b077501fd69d3d8f9810b7acbf01d3aaa004
Edward Tanguay (windows) <edward@tanguay.info>
2022-08-30 09:54:24 +0200

    zustand state variable made

---
+++ src/store.tsx
@@ -0,0 +1,14 @@
+import create from 'zustand';
+
+interface IStore {
+	message: string;
+	setMessage: (message: string) => void;
+}
+
+export const useStore = create<IStore>(
+	(set): IStore => ({
+		message: 'test',
+		setMessage: (message: string) =>
+			set((state) => ({ ...state, message })),
+	})
+);
