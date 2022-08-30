519424dd6617159a37ea5684870791c9ee38bfb1
Edward Tanguay (windows) <edward@tanguay.info>
2022-08-30 09:37:28 +0200

    state input in

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
-import './App.scss'
+import { useState } from 'react';
+import './App.scss';
 
 function App() {
-  return (
-    <div className="App">
-    <h1>Zustand Demo Site</h1>
-    <p>testing</p>
-    </div>
-  )
+	const [message, setMessage] = useState('');
+
+	return (
+		<div className="App">
+			<h1>Zustand Demo Site</h1>
+			<main>
+				<section className="controlArea">
+					<div className="data">
+						<label>Message:</label>
+						<input type="text" value={message} onChange={(e) => setMessage(e.target.value)} />
+						<div>{message}</div>
+					</div>
+				</section>
+			</main>
+		</div>
+	);
 }
 
-export default App
+export default App;
